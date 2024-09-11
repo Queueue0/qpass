53ddce55ebd74b8fe84783a83d96d32fd59fc54d
Queueue0 <jcoffey97@gmail.com>
2024-09-11 15:30:37 -0400

    added validator package

--- cmd/client/gui.go
+++ cmd/client/gui.go
@@ -14,6 +14,7 @@ import (
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"github.com/Queueue0/qpass/internal/models"
+	"github.com/Queueue0/qpass/internal/validator"
 	"github.com/tiagomelo/go-clipboard/clipboard"
 )
 
@@ -380,6 +381,7 @@ func (a *Application) addView(w *app.Window) error {
 	var userName widget.Editor
 	var password widget.Editor
 	var addBtn widget.Clickable
+	var v validator.Validator
 
 	th := material.NewTheme()
 
@@ -390,27 +392,57 @@ func (a *Application) addView(w *app.Window) error {
 
 			if addBtn.Clicked(gtx) {
 				sn, un, pw := serviceName.Text(), userName.Text(), password.Text()
-				id, err := a.PasswordModel.Insert(*a.ActiveUser, sn, un, pw)
-				if err != nil {
-					return err
-				}
-				p := models.Password{
-					ID:          id,
-					UserID:      a.ActiveUser.ID,
-					ServiceName: sn,
-					Username:    un,
-					Password:    pw,
-				}
 
-				a.Passwords = append(a.Passwords, p)
-				a.Passwords.Sort()
-				w.Perform(system.ActionClose)
+				// Validate data
+				v = validator.Validator{}
+				v.CheckField(validator.NotBlank(sn), "service", "Service Name cannot be blank")
+
+				if v.Valid() {
+					id, err := a.PasswordModel.Insert(*a.ActiveUser, sn, un, pw)
+					if err != nil {
+						return err
+					}
+					p := models.Password{
+						ID:          id,
+						UserID:      a.ActiveUser.ID,
+						ServiceName: sn,
+						Username:    un,
+						Password:    pw,
+					}
+
+					a.Passwords = append(a.Passwords, p)
+					a.Passwords.Sort()
+					w.Perform(system.ActionClose)
+				}
 			}
 
 			layout.Flex{
 				Axis:    layout.Vertical,
 				Spacing: layout.SpaceEnd,
 			}.Layout(gtx,
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						if v.Valid() {
+							return layout.Dimensions{}
+						}
+						errTxt, in := v.FieldErrors["service"]
+						if !in {
+							return layout.Dimensions{}
+						}
+						
+						txt := material.Body1(th, errTxt)
+						txt.Color = color.NRGBA{R: 244, G: 67, B: 54, A: 255}
+
+						margins := layout.UniformInset(unit.Dp(10))
+						margins.Bottom = 0
+						return margins.Layout(gtx,
+							func(gtx layout.Context) layout.Dimensions {
+								return txt.Layout(gtx)
+							},
+						)
+
+					},
+				),
 				layout.Rigid(
 					func(gtx layout.Context) layout.Dimensions {
 						txt := material.Editor(th, &serviceName, "Service Name")
---
+++ internal/validator/validator.go
@@ -0,0 +1,36 @@
+package validator
+
+import "strings"
+
+type Validator struct {
+	NonFieldErrors []string
+	FieldErrors    map[string]string
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddFieldError(key, message string) {
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+
+	if _, in := v.FieldErrors[key]; !in {
+		v.FieldErrors[key] = message
+	}
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
+
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+func NotBlank(s string) bool {
+	return strings.TrimSpace(s) != ""
+}
